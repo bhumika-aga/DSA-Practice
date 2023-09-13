c4ac3fcd4970675bebeba5b9391eb27021f3a2e7
bhumika-aga <bhumika.aga@gmail.com>
2023-09-13 20:10:53 +0530

    Updated Code

--- 2DArrays/PepCoding/ExitPointOfAMaze.java
+++ 2DArrays/PepCoding/ExitPointOfAMaze.java
@@ -2,9 +2,28 @@ package PepCoding;
 
 import java.util.Scanner;
 
+/* 
+ * Given a 2D Array Maze of 0's and 1's print the exit point of the maze.
+ * The constraints are such that seeing zero's, the pointer will go straight, and when encountered with 1,  *the pointer takes a 90 degree turn. 
+ * Example: A[4][4] ==>
+ * [0  0  1  0]
+ * [1  0  0  0]
+ * [0  0  0  0]
+ * [1  0  1  0]
+ * The pointer's path ==> A[0][0] -> A[0][1] -> A[0][2] -> A[1][2] -> A[2][2] -> A[3][2] -> A[3][1] -> A[3][0] -> A[2][0] -> A[1][0] -> A[1][1] -> A[1][2] -> A[1][3]
+ * Output -> A[1][3] (Exit Point of the Maze)
+*/
 public class ExitPointOfAMaze {
     public static int[] exitPoint(int rows, int columns, int[][] arr) {
-        return null;
+        int currRow = 0;
+        int currCol = 0;
+        int[] exitPointMatrix = { 0, 0 };
+        if (arr[currRow][currCol] == 1) {
+            currRow++;
+        } else {
+            currCol++;
+        }
+        return exitPointMatrix;
     }
 
     public static void main(String[] args) {
--- 2DArrays/PepCoding/SpiralTraversal.java
+++ 2DArrays/PepCoding/SpiralTraversal.java
@@ -3,11 +3,12 @@ package PepCoding;
 import java.util.Scanner;
 
 /* 
- * Given a 2D Array print the array in a wave form. 
+ * Given a 2D Array print the array in a spiral form. 
  * Example: A[2][3] ==>
  * [1  3  5]
  * [2  4  6]
  * Output ==> 1, 2, 4, 6, 5, 3
+ * The approach dictates to print the outer most elements of the matrix first followed by the inside ones.
 */
 public class SpiralTraversal {
     /*
@@ -15,10 +16,56 @@ public class SpiralTraversal {
      * We print the outer-most box first, then the one inside it and so on...
      */
     public static void spiralTraversal(int rows, int columns, int[][] arr) {
-        /* 
-         * 
+        /*
+         * Now, to identify a box, we only need the top left and bottom right dimension
+         * of it.
+         * We need four loops to print one box - 1. For left wall, 2. For bottom wall,
+         * 3. For Right wall, and 4. For top wall.
+         * For the left wall and right walls, the column will remain same and the rows
+         * will change from min to max, and vice versa.
+         * For the bottom and top walls, the column stays the same and the rows change
+         * from min to max, and vice versa.
+         * TO save from repeating corners we increase minCol and minRow, and decrease
+         * maxRow and maxCol. as soon as they are printed.
          */
-        return;
+        // Defines the top left dimension of the box.
+        int minRow = 0;
+        int minCol = 0;
+        // Defines the bottom right dimension of the box.
+        int maxRow = arr.length - 1;
+        int maxCol = arr[0].length - 1;
+
+        // To check that we have printed all the elements, no more or less.
+        int totalElements = rows * columns;
+        int counter = 0;
+
+        // While any box is left
+        while (counter < totalElements) {
+            // Left wall
+            for (int i = minRow, j = minCol; i <= maxRow && counter < totalElements; i++) {
+                System.out.print(arr[i][j] + " ");
+                counter++;
+            }
+            minCol++;
+            // Bottom wall
+            for (int i = maxRow, j = minCol; j <= maxCol && counter < totalElements; j++) {
+                System.out.print(arr[i][j] + " ");
+                counter++;
+            }
+            maxRow--;
+            // Right wall
+            for (int i = maxRow, j = maxCol; i >= minRow && counter < totalElements; i--) {
+                System.out.print(arr[i][j] + " ");
+                counter++;
+            }
+            maxCol--;
+            // Top wall
+            for (int i = minRow, j = maxCol; j >= minCol && counter < totalElements; j--) {
+                System.out.print(arr[i][j] + " ");
+                counter++;
+            }
+            minRow++;
+        }
     }
 
     public static void main(String[] args) {
